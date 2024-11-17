d83861b7a06507ab72b788f13b5be112ececf77c
stinnovx <salauddin.innovx@gmail.com>
2024-11-17 15:17:41 +0600

    notice board and notice board details updated

--- resources/views/pages/noticeboardDetails.blade.php
+++ resources/views/pages/noticeboardDetails.blade.php
@@ -6,23 +6,44 @@
 @section('header-class', 'header-transparent')
 
 
+@section('content')
+
+<div class="noticeboard-header">
+    <h1 class="noticeboard-title">Noticeboard</h1>
+</div>
+
+<?php
+    $titleArray=[
+        'Sports day preparation event calling',
+        'Picnic registration is now open',
+        'Semester exam date postponed',
+        'Library building renovated'
+        ];
+
+?>
 
 
 
 <section class="about-section">
     <div class="about-content">
         <div class="text-content">
-            <h5> About</h5>
-            <h2>Welcome to Ekattor High School</h2>
-            <p>Ekattor High School (NHS) is a public secondary school in Bellevue, Washington. It serves students in
-                grades 9–12 in the southern part of the Bellevue School District, including the neighborhoods of
-                Eastgate, Factoria, Newport Hills, Newport Shores, Somerset, The Summit, and Sunset. As of the 2014-2015
-                school year, the principal is Dion Yahoudy. The mascot is the Knight, and the school colors are scarlet
-                and gold.</p>
-            <button class="learn-more-btn">Learn More</button>
+            <a href="{{url('/noticeboard')}}" class="back-button">
+                <span class="fas fa-arrow-left small back-btn-icon"></span>
+                Back to noticeboard</a>
+            <h2>{{$titleArray[$title-1]}}</h2>
+
+            <p class="">31 Dec, 2019</p>
+
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard 
+                McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+                the cites of the word in classical literature, discovered the undoubtable source.
+            </p>
         </div>
+
         <div class="image-content">
-            <img src="{{ asset('images/home_promo_1.png') }}" alt="Ekattor High School" />
+            <img src="{{ asset('images/nb1.jpg') }}" alt="Ekattor High School" />
         </div>
     </div>
 </section>
+@endsection
+
