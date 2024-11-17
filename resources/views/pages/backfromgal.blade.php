01451124855fb64543db0ec91df8220e0a5dd562
sobuj <mdsobujshu2001@gmil.com>
2024-11-17 17:32:05 +0600

    test

--- resources/views/pages/backfromgal.blade.php
+++ resources/views/pages/backfromgal.blade.php
@@ -18,8 +18,8 @@
         reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem
         Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content
         here', making it look like readable English.</p>
-    <div class="gal-container">
 
+    <div class="gal-container">
         <div class="pic">
             <img src="{{ asset('images/picg5.jpg') }}" class="vlog-pic">
             <div class="plus-overlay"> <span class="plus-icon">+</span> </div>
