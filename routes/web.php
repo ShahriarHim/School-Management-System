0b879e31c8f6721a6bb5e8880fdff9a2247fe354
stinnovx <salauddin.innovx@gmail.com>
2024-11-18 12:05:21 +0600

    Revert "notice board and notice board details updated"
    This reverts commit d83861b7a06507ab72b788f13b5be112ececf77c.

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ Route::get('/', function () {
 Route::get('/noticeboard', function () {
     return view('pages.noticeboard');
 });
-Route::get('/noticeboard-details/{title}', function ($title) {
-    return view('pages.noticeboardDetails',['title'=>$title]);
-})->name('noticeboard-details');
+Route::get('/noticeboard-details', function () {
+    return view('pages.noticeboardDetails');
+});
 
 Route::get('/about', function(){
     return view('pages.about');
