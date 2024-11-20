7d9eda05a7406fa6240cea89b846f1c321da9a37
ShahriarHim <shahriarhim19@gmail.com>
2024-11-20 12:58:56 +0600

    Adjusted the file structure

---
+++ resources/views/admin/noticeManagement.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('title', 'Noticeboard Management Panel')
+
+@section('content')
+<link rel="stylesheet" href="{{ asset('css/noticeManagement.css') }}">
+<div class="management-panel">
+    <h1 class="panel-header">Noticeboard Management Panel</h1>
+    
+    <form action="{{ route('admin.noticeboard.store') }}" method="POST" enctype="multipart/form-data" class="notice-form">
+        @csrf
+        <div class="form-group">
+            <label for="title">Notice Title</label>
+            <input type="text" name="title" id="title" class="form-control" placeholder="Enter the notice title" required>
+        </div>
+
+        <div class="form-group">
+            <label for="date">Notice Date</label>
+            <input type="date" name="date" id="date" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+            <label for="image">Notice Image</label>
+            <input type="file" name="image" id="image" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="description">Notice Description</label>
+            <textarea name="description" id="description" rows="5" class="form-control" placeholder="Enter the notice description"></textarea>
+        </div>
+
+        <div class="form-actions">
+            <button type="submit" class="btn btn-primary">Save Notice</button>
+            <button type="reset" class="btn btn-secondary">Clear</button>
+        </div>
+    </form>
+</div>
+@endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -95,7 +95,7 @@
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="/admin/notice-management" class="nav-link">
               <i class="nav-icon fas fa-bullhorn"></i>
               <p>
                 Notice Management
@@ -144,7 +144,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#" id="homedes">Home</a></li>
+              <li class="breadcrumb-item"><a href="/" id="homedes">Home</a></li>
             </ol>
           </div>
         </div>
