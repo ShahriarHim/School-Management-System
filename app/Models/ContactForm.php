fa55f13e0e85ab07e86a877083bcb917ffbf2251
stinnovx <salauddin.innovx@gmail.com>
2024-11-20 18:11:43 +0600

    ContactForm model fixed(previously corrupted by sobuj

--- app/Models/ContactForm.php
+++ app/Models/ContactForm.php
@@ -5,20 +5,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
-class Event extends Model
+class ContactForm extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'title',
-        'author_name',
-        'date',
-        'status',
-        'image',
-        'description'
-    ];
-
-    protected $casts = [
-        'date' => 'datetime',
-    ];
 }
