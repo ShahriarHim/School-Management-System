f6c8e7632095ae6941d85ada527acbe816caf6af
stinnovx <salauddin.innovx@gmail.com>
2024-11-21 13:35:38 +0600

    fixed pageContent text-editor and extended it from the admin

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::resource('admin/school', SchoolDetailController::class)->names('admin.sch
 
 
 
-//----------Salauddin's route end------------
+//----------Salauddin's route ends here------------
 
 
 //Sobuj Part
