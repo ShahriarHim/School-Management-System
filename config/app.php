c928c59baab7b1d23d8f30fe0d5264e40d22ed10
ShahriarHim <shahriarhim19@gmail.com>
2024-11-28 16:27:07 +0600

    OrderBy

--- config/app.php
+++ config/app.php
@@ -182,10 +182,11 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
-
+        
         /*
          * Application Service Providers...
          */
